565fc4648847a6bb71e1aa44ddc19aa235739233
kellyhu0109 <kellyhu0109@gmail.com>
2019-12-03 15:38:26 +0800

    add new file bot/forms.py

---
+++ code/bot/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, Form, BooleanField
+from .models import OcrTable
+
+
+class OcrModelForm(ModelForm):
+    class Meta:
+        model = OcrTable
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(OcrModelForm, self).__init__(*args,**kwargs)
+        self.fields['ocr_no'].widget.attrs['readonly'] = True
